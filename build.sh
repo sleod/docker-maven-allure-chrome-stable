45f95a5565edb06412ff39799c97228f95302058
sleod <40633990+sleod@users.noreply.github.com>
2023-09-26 12:06:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e -o pipefail
 
 # jdk-17
 
-docker build --pull -t docker-maven-allure-chrome-stable:jdk-17 jdk-17
+docker build --pull -t maven-allure-chrome-stable:jdk-17 jdk-17
 TAG=jdk-17 bats test

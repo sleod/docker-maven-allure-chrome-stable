a929e37bc9acbc7a58ec012b8d6ab15fc363a75a
sleod <40633990+sleod@users.noreply.github.com>
2024-10-24 12:05:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ set -e -o pipefail
 
 docker build --pull -t maven-allure-chrome-stable:jdk-17 jdk-17
 TAG=jdk-17 bats test
+
+
+# jdk-21
+
+docker build --pull -t maven-allure-chrome-stable:jdk-21 jdk-21
+TAG=jdk-21 bats test
